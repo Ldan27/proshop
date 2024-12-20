0554ddbe0e8f255dcfd98bd3ddf61ee2f7aaef61
MWITWA KATANTI DAN <danielkatanti@gmail.com>
2024-12-20 08:03:05 +0200

    middleware added successfully

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './Routes/productRoutes.js';
+import userRoutes from './Routes/userRoutes.js';
 const port = process.env.PORT || 5000;
 dotenv.config();
 
@@ -10,11 +12,19 @@ connectDB(); // Connect to mongoDB
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json()); // parse raw json
+app.use(express.urlencoded({ extended: true })); // Parse the form urlencoded in the body
+
+// Cookie parser middleware
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
