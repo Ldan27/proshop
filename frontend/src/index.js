b6b0803b5090c6e5a424a98bc0dcaa5c6fc37d5d
MWITWA KATANTI DAN <danielkatanti@gmail.com>
2024-12-27 15:16:25 +0200

    payOrder && getPayPalClientId added!!!

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,7 @@ import {
   Route,
   RouterProvider,
 } from 'react-router-dom';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
@@ -47,7 +48,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <PayPalScriptProvider deferLoading={true}>
+        <RouterProvider router={router} />
+      </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
