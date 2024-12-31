c239059f1116eedc71c41e56f08015b2813cc955
MWITWA KATANTI DAN <danielkatanti@gmail.com>
2024-12-31 09:01:50 +0200

    createProduct added to slice

--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -15,8 +15,18 @@ export const productsApiSlice = apiSlice.injectEndpoints({
       }),
       keepUnusedDataFor: 5,
     }),
+    createProduct: builder.mutation({
+      query: () => ({
+        url: PRODUCTS_URL,
+        method: 'POST',
+      }),
+      invalidatesTags: ['Product'],
+    }),
   }),
 });
 
-export const { useGetProductsQuery, useGetproductDetailsQuery } =
-  productsApiSlice;
+export const {
+  useGetProductsQuery,
+  useGetproductDetailsQuery,
+  useCreateProductMutation,
+} = productsApiSlice;
