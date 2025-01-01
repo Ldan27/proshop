aef62302384ca27205d505d214db7d70698b9f4b
MWITWA KATANTI DAN <danielkatanti@gmail.com>
2025-01-01 10:20:47 +0200

    upload folder added!

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import connectDB from './config/db.js';
 import dotenv from 'dotenv';
@@ -6,6 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './Routes/productRoutes.js';
 import userRoutes from './Routes/userRoutes.js';
 import orderRoutes from './Routes/orderRoutes.js';
+import uploadRoutes from './Routes/uploadRoutes.js';
 const port = process.env.PORT || 5000;
 dotenv.config();
 
@@ -27,10 +29,14 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+const __dirname = path.resolve(); // Set __dirname to current directory
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(notFound);
 app.use(errorHandler);
