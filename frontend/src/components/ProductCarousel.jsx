4fdc178d2e45e62a6eb769d1c84275d16fe477e2
MWITWA KATANTI DAN <danielkatanti@gmail.com>
2025-01-04 07:09:06 +0200

    route for carousel created && carousel page

---
+++ frontend/src/components/ProductCarousel.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import { Carousel, Image } from 'react-bootstrap';
+import Loader from './Loader';
+import Message from './Message';
+import { useGetTopProductsQuery } from '../slices/productsApiSlice';
+
+import React from 'react';
+
+const ProductCarousel = () => {
+  return <div>ProductCarousel</div>;
+};
+
+export default ProductCarousel;
