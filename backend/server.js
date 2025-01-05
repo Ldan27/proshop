8a82da204c6367a1d37dcba66c0341d1983b42b1
MWITWA KATANTI DAN <danielkatanti@gmail.com>
2025-01-05 19:31:49 +0200

    file fixed !!!

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true })); // Parse the form urlencoded in
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -39,8 +35,9 @@ const __dirname = path.resolve(); // Set __dirname to current directory
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 if (process.env.NODE_ENV === 'production') {
+  console.log(`hi you're good!!!`);
   // set static folder
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, 'frontend', 'build')));
 
   // any route that is not api will be redirected to index.html
   app.get('*', (req, res) =>
